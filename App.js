c097f77d369aa8d303321873f62bf5d612591205
gaurav <gaurav@acmeminds.com>
2021-08-10 11:43:32 +0530

    now in this we did the exact same but in function component

--- App.js
+++ App.js
@@ -1,57 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { Alert, Image, SafeAreaView, FlatList, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 
-import React from 'react';
-import { SafeAreaView ,StyleSheet, Text, View, FlatList, Image, TouchableOpacity, Alert } from 'react-native';
+export default App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: []
-    };
-  }
-                        
-  componentDidMount() {                           // compnent did mount 
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then((responseJson) => {
-        this.setState({
-          dataSource: responseJson         // data - dataSource
-        })
-      })
-      .catch(error => console.log(error)) //to catch the errors if any
-  }
-
-// now complete your view content
-
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <FlatList
-          style={{ flex: 1 }}
-          data={this.state.dataSource}
-          renderItem={({ item }) =>
-            <View style={styles.listItem}>
-              <Image source={{ uri: 'https://image.flaticon.com/icons/png/512/1256/1256650.png' }} style={styles.imageItem}  />
-              <View style={{ alignItems: "center", flex: 1 }}>
-                <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
-                <Text>{item.email}</Text>
-                <Text>{item.company.name}</Text>
-              </View>
-              <TouchableOpacity onPress={() => Alert.alert('Phone No :'+ item.phone)}  style={styles.button}>   
-               <Text style={{ color: "green" }}>Call</Text>
-              </TouchableOpacity>
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        style={{ flex: 1 }}
+        data={data}
+        renderItem={({ item }) =>
+          <View style={styles.listItem}>
+            <Image source={{ uri: 'https://img-premium.flaticon.com/png/512/1165/premium/1165674.png?token=exp=1628513272~hmac=a94f95966a5725122c52ae188e430cf1' }} style={styles.imageItem} />
+            <View style={{ alignItems: "center", flex: 1 }}>
+              <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+              <Text>{item.email}</Text>
+              <Text>{item.company.name}</Text>
             </View>
-          }
+            <TouchableOpacity onPress={() => Alert.alert('Phone No :' + item.phone)} style={styles.button}>
+              <Text style={{ color: "green" }}>Call</Text>
+            </TouchableOpacity>
+          </View>
+        }
 
-          keyExtractor={item => item.id}
-        />
-      </SafeAreaView>
-    );
-  }
-
-
-}
+        keyExtractor={(item, index) => index}
+      />
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
